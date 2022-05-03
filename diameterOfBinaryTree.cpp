744250afebf86cb9e9a48567c55da0690bd8e968
srishti0202 <104730353+srishti0202@users.noreply.github.com>
2022-05-04 01:49:48 +0530

    Added code with better time complexity

--- diameterOfBinaryTree.cpp
+++ diameterOfBinaryTree.cpp
@@ -15,3 +15,33 @@ public:
         return ans;
     }
 };
+
+//Improved Time Complexity - decreasing the number of calls to the height function
+
+
+class Solution {
+public:
+    
+    pair<int,int> diameterHelper(TreeNode* root){
+        pair<int,int> ans;
+        if(root==NULL){
+            ans.first=0;
+            ans.second=0;
+            return ans;
+        }
+        pair<int,int> left=diameterHelper(root->left);
+        pair<int,int> right=diameterHelper(root->right);
+        int leftHeight=left.first;
+        int leftDiameter=left.second;
+        int rightHeight=right.first;
+        int rightDiameter=right.second;
+        ans.first=1+max(leftHeight, rightHeight);
+        ans.second=max(leftHeight+rightHeight, max(leftDiameter, rightDiameter));
+        return ans;
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL) return 0;
+        return diameterHelper(root).second;
+    }
+};
