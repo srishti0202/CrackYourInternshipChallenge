c151eb23bb0a9a971d8251b1e962de683f0ea6c5
srishti0202 <104730353+srishti0202@users.noreply.github.com>
2022-05-13 22:58:14 +0530

    Create recoverBinaryTree.cpp

---
+++ recoverBinaryTree.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+private:
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+public:
+    
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        if(prev!=NULL && root->val < prev->val){
+            if(first==NULL){
+                first=prev;
+                middle=root;
+            }
+            else{
+                last=root;
+                
+            }
+        }
+         prev=root;
+         inorder(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        if(!root) return;
+        first=middle=last=NULL;
+        prev=new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val, last->val);
+        else if(first && middle) swap(first->val, middle->val);
+    }
+};
